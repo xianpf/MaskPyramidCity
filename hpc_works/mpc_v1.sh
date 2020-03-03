07ad3646e4e1d6d21dc8fd39c750628e5f591e7e
xianr_jx <xian.pf@my.cityu.edu.hk>
2020-03-03 17:34:30 +0800

    compatible run with hpc settings

--- hpc_works/mpc_v1.sh
+++ hpc_works/mpc_v1.sh
@@ -4,6 +4,6 @@
 #SBATCH --job-name=mpc_xpf
 #SBATCH --gres=gpu:1
 
-mpirun python train_files/mpc_v1.py
+mpirun python train_files/mpc_v1.py --config hpc_works/HpcMaskPyramidV1.yml
 
 # sbatch train_files/mpc_v1.py

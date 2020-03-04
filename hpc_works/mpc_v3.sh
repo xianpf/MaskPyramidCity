e7c166660d99d8bd41e95fff8ae0af9b8c84bae4
xianr_jx <xian.pf@my.cityu.edu.hk>
2020-03-04 20:53:47 +0800

    shell for v3

---
+++ hpc_works/mpc_v3.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#SBATCH -o /home/jingxiong9/xpf/MaskPyramidCity/hpc_works/mpc_v3.out
+#SBATCH --job-name=mpc_xpf
+#SBATCH --gres=gpu:1
+
+mpirun python train_files/mpc_v3.py --config hpc_works/HpcMaskPyramidV1.yml
+
+# MODEL.DEVICE cpu
+# sbatch hpc_works/mpc_v3.sh
